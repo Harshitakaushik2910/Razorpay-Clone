b88b0d606c0ae434795131ccb38664e45891da57
Harshitakaushik2910 <h.kaushik1029@gmail.com>
2024-07-25 22:44:07 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lightBlue:"#2b84ea",
+        lightBlue300:"#4b94ed",
+        lightBlue800:"#0b727e7",
+        greenLight:"#61cea6",
+        grayText:"#818597",
+        lightGray:"#e2e2e2",
+        grayBlue:"#344a6c",
+        DeepBlueHead:"#162f56",
+        gray2:"#525a76",
+        haha:"#181c2e",
+        bggray:"#E8E8E8",
+        billu:"#02ccfe",
+        green:"#69fe0f"
+      },
+    },
+  },
+  plugins: [],
+}
+
